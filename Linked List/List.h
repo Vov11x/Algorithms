c70f72e58abfac373533047047a2bcc0e3d4ae64
Vova Makotkin <66742594+Vov11x@users.noreply.github.com>
2021-02-01 20:00:20 +0200

    Update List.h

--- Linked List/List.h
+++ Linked List/List.h
@@ -18,13 +18,13 @@ public:
 
     List();
     ~List();
-    void push_back(T data);
-    int size() { return size_; }
-    T &operator[](int index);
-    void pop_front();
-    void clear();
-    void push_front(T data);
-    void insert(T data, int index);
-    void remove(int index);
-    void pop_back();
+    void push_back(T data);  // adds element in the end of list
+    int size() { return size_; }  // returns size of the list
+    T &operator[](int index);  // [] operator overloading
+    void pop_front();  // deletes first element of the list
+    void clear();  // clearing of the list
+    void push_front(T data);  // adds element in beginning of the list
+    void insert(T data, int index);  // inserts one element at defined index
+    void remove(int index);  // removes element at index
+    void pop_back();  // removes last element
 };
